50e457c6963d3583add24892eb1e39f17505196a
yogaprnt <154289412+yogaprnt@users.noreply.github.com>
2024-03-19 23:58:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function calc()
+            {
+                var n1 = parseFloat(document.getElementById('n1').value);
+                var n2 = parseFloat(document.getElementById('n2').value);
+                
+                var oper = document.getElementById('operators').value;
+                
+                if(oper === '+')
+                {
+                    document.getElementById('result').value = n1+n2;
+                }
+                
+                if(oper === '-')
+                {
+                    document.getElementById('result').value = n1-n2;
+                }
+                
+                if(oper === '/')
+                {
+                    document.getElementById('result').value = n1/n2;
+                }
+                
+                if(oper === 'X')
+                {
+                    document.getElementById('result').value = n1*n2;
+                }
+                if(oper === '%')
+                {
+                    document.getElementById('result').value = n1%n2;
+                }
+            }
